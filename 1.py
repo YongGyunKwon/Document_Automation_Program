96096eaedc2478cb2606c2aa21116ec17c4779d2
YongGyunKwon <ygkwon50@gmail.com>
2021-01-12 13:51:28 +0900

    make function

--- 1.py
+++ 1.py
@@ -1,33 +1,77 @@
 from docx.api import Document
-#from xlsxwriter import Workbook
+from xlsxwriter import Workbook
 import pandas as pd 
+from pandas import DataFrame
+import io
+#import Workbook
 
 path= "C:/Users/ygkwo/Desktop/test1/1.docx" #input_path
-document=Document(path)
 output_path="C:/Users/ygkwo/Desktop/test1" #output_path
 
-
-#writer=pd.ExcelWriter('{}/docx_talbes.xlsx'.format(output_path), engine='xlsxwriter')
-
+# document=Document(path)
 
 
-for i in range(len(document.tables)):
-    table=document.tables[i]
-    data=[]
-    keys=None
-    row_data=None
+# writer=pd.ExcelWriter('{}/docx_talbes.xlsx'.format(output_path),engine='xlsxwriter')
+
+
+
+# for i in range(len(document.tables)):
+#     table=document.tables[i]
+#     data=[]
+#     keys=None
+#     row_data=None
     
-    for j,row in enumerate(table.rows):
-        text=(cell.text for cell in row.cells)
-        if j==0:
-            keys=tuple(text)
-            continue
-        row_data=dict(zip(keys,text))
-        data.append(row_data)
+#     for j,row in enumerate(table.rows):
+#         text=(cell.text for cell in row.cells)
+#         if j==0:
+#             keys=tuple(text)
+#             continue
+#         row_data=dict(zip(keys,text))
+#         data.append(row_data)
+#         #
+#         # df1=pd.DataFrame(data)
+#         # print(df1)
+#         # df1.to_excel(writer,sheet_name='table_{}'.format(i))
 
-    df=pd.DataFrame(data)
-    print(df)
-    df.to_excel(writer,sheet_name='table_{}'.format(i))
+#     df=pd.DataFrame(data)
+#     print(df)
+#     df.to_excel(writer,sheet_name='table_{}'.format(i))
 
 
-#writer.save()
+# writer.save()
+
+
+
+#docx_to_xlsx Function [Input, Output]
+def docx_to_xlsx(input,output):
+    document=Document(path)
+
+    writer=pd.ExcelWriter('{}/docx_talbes.xlsx'.format(output_path),engine='xlsxwriter')
+
+    for i in range(len(document.tables)):
+        table=document.tables[i]
+        data=[]
+        keys=None
+        row_data=None
+        
+        for j,row in enumerate(table.rows):
+            text=(cell.text for cell in row.cells)
+            if j==0:
+                keys=tuple(text)
+                continue
+            row_data=dict(zip(keys,text))
+            data.append(row_data)
+            #
+            # df1=pd.DataFrame(data)
+            # print(df1)
+            # df1.to_excel(writer,sheet_name='table_{}'.format(i))
+
+        df=pd.DataFrame(data)
+        print(df)
+        df.to_excel(writer,sheet_name='table_{}'.format(i))
+
+
+    writer.save()
+
+
+docx_to_xlsx(path,output_path)
