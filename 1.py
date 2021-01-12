d1c78b2121e86386e3d186d08a01eb717f79aad2
YongGyunKwon <ygkwon50@gmail.com>
2021-01-12 15:04:49 +0900

    modify 1.py

--- 1.py
+++ 1.py
@@ -3,13 +3,17 @@ from xlsxwriter import Workbook
 import pandas as pd 
 from pandas import DataFrame
 import io
+import Document_Automation_Program.folder_finding
+
 #import Workbook
 
 path= "C:/Users/ygkwo/Desktop/test1/1.docx" #input_path
 output_path="C:/Users/ygkwo/Desktop/test1" #output_path
 
-# document=Document(path)
 
+#original
+
+# document=Document(path)
 
 # writer=pd.ExcelWriter('{}/docx_talbes.xlsx'.format(output_path),engine='xlsxwriter')
 
@@ -42,11 +46,15 @@ output_path="C:/Users/ygkwo/Desktop/test1" #output_path
 
 
 
+
+
+
+
 #docx_to_xlsx Function [Input, Output]
 def docx_to_xlsx(input,output):
     document=Document(path)
 
-    writer=pd.ExcelWriter('{}/docx_talbes.xlsx'.format(output_path),engine='xlsxwriter')
+    writer=pd.ExcelWriter('{}/docx_talbes.xlsx'.format(output),engine='xlsxwriter')
 
     for i in range(len(document.tables)):
         table=document.tables[i]
@@ -74,4 +82,38 @@ def docx_to_xlsx(input,output):
     writer.save()
 
 
+
+def docx_to_xlsx_multi(input,output):
+    document=Document(path)
+
+    writer=pd.ExcelWriter('{}/docx_talbes.xlsx'.format(output),engine='xlsxwriter')
+
+    for i in range(len(document.tables)):
+        table=document.tables[i]
+        data=[]
+        keys=None
+        row_data=None
+        
+        for j,row in enumerate(table.rows):
+            text=(cell.text for cell in row.cells)
+            if j==0:
+                keys=tuple(text)
+                continue
+            row_data=dict(zip(keys,text))
+            data.append(row_data)
+            #
+            # df1=pd.DataFrame(data)
+            # print(df1)
+            # df1.to_excel(writer,sheet_name='table_{}'.format(i))
+
+        df=pd.DataFrame(data)
+        print(df)
+        df.to_excel(writer,sheet_name='table_{}'.format(i))
+
+
+    writer.save()
+
+
+
+#one docx file
 docx_to_xlsx(path,output_path)
